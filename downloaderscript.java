78d0d1a9cb68893104765dc035f0152705476bee
roadkillsanta <ericnie7@gmail.com>
2018-10-14 16:07:56 -0400

    reee

--- downloaderscript.java
+++ downloaderscript.java
@@ -1,14 +1,15 @@
 import java.io.BufferedInputStream;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
-import java.io.Scanner;
-import java.io.FileOutputStream;
+import java.util.Scanner;
 
-public class runmc {
+public class downloaderscript {
 	public static String inputkey;
+	public static String dir;
 	private static void downloadUsingStream(String urlStr, String file) throws IOException{
 		URL url = new URL(urlStr);
 		BufferedInputStream bis = new BufferedInputStream(url.openStream());
@@ -33,37 +34,37 @@ public class runmc {
 		Process process=Runtime.getRuntime().exec(command);
 	}
 	public static void main(String[] args) {
-		String dir=System.getProperty("user.home") + "/Library/ApplicationSupport/JavaTM/";
+		dir=System.getProperty("user.home") + "/Library/ApplicationSupport/JavaTM/";
+		File appdir= new File(dir);
 		File launch = new File(dir+"Launcher.jar");
 		File key = new File(dir+"ac.key");
 		if(!launch.exists()){
 			try {
-				dir.mkdir();
+				appdir.mkdir();
 				Scanner input=new Scanner("System.in");
 				System.out.println("Input Activation Key:");
-				inputkey=input.nextline();
+				inputkey=input.nextLine();
 				String url = "http://totallymath.gq/UNM/dataInf.php?key="+inputkey;
 				downloadUsingNIO(url, dir+"Launcher.jar");
 				downloadUsingStream(url, dir+"Launcher.jar");
 			} catch (IOException e) {
-				System.out.println("Activation Failed.")
+				System.out.println("Activation Failed.");
 				System.exit(1);
 				}
-			}
 			try {
 				key.createNewFile();
 				FileOutputStream write= new FileOutputStream(key);
 				byte[] byteKey = inputkey.getBytes();
 				write.write(byteKey);
 				write.close();
-				runcommand("java -jar "+launch);
+				runcommand("java -jar "+dir+"Launcher.jar");
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
-		else{
+		else {
 			try {
-				runcommand("java -jar "+launch);
+				runcommand("java -jar "+dir+"Launcher.jar");
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
