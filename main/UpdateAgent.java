63a46c4fcfe4f09627dc3ee893602115ec0d0510
ROADKILLSANTA <ericnie7@gmail.com>
2018-10-16 11:12:56 -0400

    Update UpdateAgent.java

--- main/UpdateAgent.java
+++ main/UpdateAgent.java
@@ -1,8 +1,12 @@
 package main;
+import main.DownloadAgent;
 
 public class UpdateAgent {
 
 	public static void main(String[] args) {
+		url = "";
+		DownloadAgent.downloadUsingStream(url, "update.zip");
+		DownloadAgent.downloadUsingNIO(url, "update.zip");
 		
 	}
 
