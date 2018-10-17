6d78bbe4dd2d5efe9e5d50c4bdeee3d9a28635a6
ROADKILLSANTA <ericnie7@gmail.com>
2018-10-17 08:02:25 -0400

    Update loader.java

--- main/loader.java
+++ main/loader.java
@@ -7,6 +7,21 @@ public class loader {
 	public static void runcommand(String command) throws IOException{
 		Runtime.getRuntime().exec(command);
 	}
+	public void restartApplication(){
+		final String javaBin = System.getProperty("java.home") + File.separator + "bin" + File.separator + "java";
+		final File currentJar = new File(MyClassInTheJar.class.getProtectionDomain().getCodeSource().getLocation().toURI());
+		/* is it a jar file? */
+		if(!currentJar.getName().endsWith(".jar"))
+		return;
+		/* Build command: java -jar application.jar */
+		final ArrayList<String> command = new ArrayList<String>();
+		command.add(javaBin);
+		command.add("-jar");
+		command.add(currentJar.getPath());
+		final ProcessBuilder builder = new ProcessBuilder(command);
+		builder.start();
+		System.exit(0);
+	}
 	public static void main(String[] args) {
 		File key = new File(dir+"ac.key");
 		if(!key.exists()){
