bea45a365324024bf1dbcf4bf8a7a7a2d2026040
ROADKILLSANTA <ericnie7@gmail.com>
2018-10-17 07:31:03 -0400

    Create loading.java

---
+++ GUI/loading.java
@@ -0,0 +1,19 @@
+package GUI;
+import javax.swing.*;
+import GUI.buildWindow;
+
+public class loading{
+	public void LoadingFrame(){
+		JFrame frame = new JFrame("Load");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JLabel label = new JLabel("JavaTM Launcher");
+		frame.getContentPane().add(label);
+		frame.pack();
+		frame.setVisible(true);
+	}
+	public static void main(String[] args) {
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {createAndShowGUI();}
+		});
+	}
+}
