19cc206a9e9eec6da7740a8c408cdb525dc1af5f
ROADKILLSANTA <ericnie7@gmail.com>
2018-10-19 23:42:57 -0400

    s

--- GUI/buildWindow.java
+++ GUI/buildWindow.java
@@ -9,6 +9,7 @@ import javax.swing.JFrame;
 import javax.swing.JList;
 import java.awt.Color;
 import javax.swing.JTable;
+import javax.swing.JToolBar;
 import javax.swing.JScrollPane;
 import javax.swing.JLabel;
 import java.awt.Component;
@@ -19,25 +20,34 @@ import javax.swing.JMenuItem;
 
 public class buildWindow extends JPanel {
 	private static JTable table;
-
-	/**
-	 * Create the panel.
-	 */
-	public static void buildWindow(JFrame f) {
+	public buildWindow(JFrame f) {
 		JPanel panel = new JPanel();
 		panel.setLayout(new BoxLayout(panel, 0));
 		JSplitPane splitPane = new JSplitPane();
+		splitPane.setEnabled(false);
 		splitPane.setOrientation(JSplitPane.VERTICAL_SPLIT);
 		panel.add(splitPane);
 		
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.LEFT);
 		splitPane.setRightComponent(tabbedPane);
+		JToolBar toolbar=new JToolBar();
+		splitPane.setLeftComponent(toolbar);
 		
+		JLabel lblJavatm = new JLabel("JavaTM");
+		toolbar.add(lblJavatm);
+		JPanel home=new JPanel();
+		JPanel yprgms=new JPanel();
+		JPanel gprgms=new JPanel();
+		JPanel tools=new JPanel();
+		JPanel settings=new JPanel();
 		table = new JTable();
 		table.setShowGrid(false);
-		tabbedPane.addTab("Launch Programs", null, table, null);
+		tabbedPane.addTab("Home", null, home, null);
+		tabbedPane.addTab("Your Programs", null, yprgms, null);
+		tabbedPane.addTab("Get Programs", null, gprgms, null);
+		tabbedPane.addTab("Tools", null, tools, null);
+		tabbedPane.addTab("Settings", null, settings, null);
 		
 		f.getContentPane().add(panel);
 	}
-
 }
--- GUI/init.java
+++ GUI/init.java
@@ -1,19 +1,20 @@
 package GUI;
+import java.awt.Dimension;
+
 import javax.swing.*;
 import GUI.buildWindow;
 
 @SuppressWarnings("serial")
 public class init extends buildWindow{
 	public init(JFrame f) {
-		super();
+		super(f);
 	}
 	public static void createAndShowGUI(){
-		JFrame frame = new JFrame("Load");
+		JFrame frame = new JFrame("JavaTM");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		JLabel label = new JLabel("JavaTM Launcher");
-		frame.getContentPane().add(label);
+		frame.setPreferredSize(new Dimension(1280,720));
 		frame.pack();
-		buildWindow(frame);
+		new buildWindow(frame);
 		frame.setVisible(true);
 	}
 	public static void main(String[] args) {
