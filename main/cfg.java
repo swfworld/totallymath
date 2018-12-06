92dc26763a8eab01c245dcfebd11c8e79275075d
roadkillsanta <ericnie7@gmail.com>
2018-12-05 23:11:18 -0500

    reree

--- main/cfg.java
+++ main/cfg.java
@@ -2,6 +2,7 @@ package main;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -12,15 +13,26 @@ public class cfg {
 	private static String cfgFile= loader.dir+"/cfg/maincfg.cfg";
 	public static final boolean rootEnabled = false;
 	public static final boolean isDev = false;
-	public cfg() {
+	public static final Hashtable<String, String> cfg=new Hashtable<String, String>();
+	public static String getProperty(String cfgProperty) {
 		if(new File(cfgFile).exists()==false) {
 			
 		}
-		Hashtable<String, String> cfg= new Hashtable<String, String>();
-		try (Stream<String> stream = Files.lines(Paths.get(cfgFile))) {
-	        stream.forEach(null);
-		} catch (IOException e) {
-			e.printStackTrace();
+		else {
+			BufferedReader reader;
+			try {
+				reader = new BufferedReader(new FileReader(cfgFile));
+				String line = reader.readLine();
+				while (line != null) {
+					cfg.put(line.split("=")[0],line.split("=")[1]);
+					line = reader.readLine();
+				}
+				reader.close();
+				return cfg.get(cfgProperty);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
+		return cfgProperty;
 	}
 }
