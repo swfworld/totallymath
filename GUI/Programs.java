e33e2de1dc8713fbbf1b7c864edda8bcb11e48b1
roadkillsanta <ericnie7@gmail.com>
2019-02-12 20:34:08 -0500

    s

---
+++ GUI/Programs.java
@@ -0,0 +1,13 @@
+package GUI;
+
+import main.JSONReader;
+
+public class Programs {
+	public static boolean getAllPrograms(){
+		
+		return false;
+	}
+	public static boolean search(String query){
+		return false;
+	}
+}
